5d3ccbe2151a2aa64cbd35b66b8b566362fdca41
Gert <gcarreon@vertere-gs.com>
2025-04-08 10:42:38 +0800

    Updated Cypress yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
-//npx cypress run --record --key 44bdf3ee-a26d-429a-9543-30d6b6acef07
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "cd78bc",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
