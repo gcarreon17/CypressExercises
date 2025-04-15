3e75a770c9169d79e0115eb136fca8ed76f54f93
Gert <gcarreon@vertere-gs.com>
2025-04-15 12:12:55 +0800

    updated report page title

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,16 @@
 const { defineConfig } = require("cypress");
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
   projectId: "cd78bc",
   experimentalStudio: true,
   //viewportWidth: 1000,
   //viewportHeight: 600,
+  defaultCommandTimeout: 10000,
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     charts: true,
-    reportPageTitle: 'custom-title',
+    reportPageTitle: 'Automation Exercise RegisterLoginDelete Report',
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false
@@ -23,7 +25,15 @@ module.exports = defineConfig({
     //baseUrl: 'https://www.saucedemo.com',
     // testIsolation: false, // Uncomment if you want to disable test isolation
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      on('before:run', async (details) => {
+        console.log('override before:run');
+        console.log('Running tests');
+        await beforeRunHook(details);
+      });
+      on('after:run', async () => {
+        console.log('override after:run');
+        await afterRunHook();
+      });
     }
   }
 });
